451ac31f3d7ea77d8878973a6eebaff6baedabec
fershad <27988517+fershad@users.noreply.github.com>
2023-04-21 18:18:15 +0800

    add Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,20 @@
+<header class="bg-white border-b-2">
+	<div class="container mx-auto px-2 sm:px-4">
+		<nav class="flex justify-between items-center bg-white py-6 md:justify-start md:space-x-10">
+			<div class="flex-grow">
+				<a href="/">
+					<img
+						width="200px"
+						height="48px"
+						src="/static/img/TGWF-logo.5fd20dfd2c0c.svg"
+						alt="The Green Web Foundation logo"
+					/>
+				</a>
+			</div>
+
+			<div class="pl-10 display-block">
+				<a href="/accounts/login/">Log in</a>
+			</div>
+		</nav>
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../app.postcss';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
+<Header />
 <slot />
