5beefe3532ebc26ea374d391d86c251d2cf2595a
fershad <27988517+fershad@users.noreply.github.com>
2023-04-21 18:01:13 +0800

    setup initial tailwind install

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-cloudflare';
 
 export default {
-    kit: {
-        adapter: adapter({
-            // See below for an explanation of these options
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>']
-            }
-        })
-    }
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
