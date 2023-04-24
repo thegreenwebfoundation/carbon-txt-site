0c3970793f197b3931230ea8c6430fad4de989ee
fershad <27988517+fershad@users.noreply.github.com>
2023-04-24 17:07:02 +0800

    add footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,22 @@
+<header class="bg-black border-t-2 mt-[10rem]">
+	<div class="container mx-auto p-4 sm:px-4">
+		<nav class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+			<div class="flex-grow">
+				<a href="/">
+					<img
+						width="200px"
+						height="48px"
+						src="/tgwf-logo.svg"
+						alt="The Green Web Foundation logo"
+					/>
+				</a>
+			</div>
+
+			<div class="pl-10 flex gap-3">
+				<a href="">What is carbon.txt?</a>
+				<a href="">Validator</a>
+				<a href="">Builder</a>
+			</div>
+		</nav>
+	</div>
+</header>
