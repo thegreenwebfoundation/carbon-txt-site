7e670a0c7848fa87fef1c65c22c31c87f3635a4d
fershad <27988517+fershad@users.noreply.github.com>
2023-04-24 17:08:48 +0800

    add code component

---
+++ src/lib/components/Code.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let lang = 'toml';
+	export let code = ';';
+	import { onMount } from 'svelte';
+	let hightlight = '';
+	onMount(async () => {
+		// const shiki = await import('shiki');
+		const highlighter = await shiki.getHighlighter({
+			theme: 'nord',
+			langs: ['toml']
+		});
+		hightlight = highlighter.codeToHtml(code, lang);
+	});
+</script>
+
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/shiki"></script>
+</svelte:head>
+
+{@html hightlight}
