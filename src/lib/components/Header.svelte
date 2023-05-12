39e5619b875b7d3ec728bf54336c9890d8cbd12e
fershad <27988517+fershad@users.noreply.github.com>
2023-05-12 21:24:54 +0800

    nicer header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,10 @@
+<script>
+	import { page } from '$app/stores'
+
+	console.log($page)
+	$: currentPage = (route = '') => ($page.url.pathname === route ? 'no-underline text-green-600' : '')
+</script>
+
 <header class="bg-white border-b-2">
 	<div class="container mx-auto px-2 sm:px-4">
 		<nav class="flex justify-between items-center bg-white py-6 md:justify-start md:space-x-10">
@@ -8,9 +15,9 @@
 			</div>
 
 			<div class="pl-10 flex gap-3">
-				<a href="/about">What is carbon.txt?</a>
-				<a href="/how/digital-services">Get started</a>
-				<a href="/tools/validator">Validator</a>
+				<a href="/about" class={currentPage('/about')}>What is carbon.txt?</a>
+				<a href="/how/digital-services" class={currentPage('/how/digital-services')}>Get started</a>
+				<a href="/tools/validator" class={currentPage('/tools/validator')}>Validator</a>
 				<a href="">Builder</a>
 				<a href="/faq">FAQ</a>
 			</div>
