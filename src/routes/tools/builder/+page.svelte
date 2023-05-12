bb0a1f75ff91b3ee3f0ff77eb575a188faaa8874
fershad <27988517+fershad@users.noreply.github.com>
2023-05-12 21:29:44 +0800

    create builder page placeholder

---
+++ src/routes/tools/builder/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	// Components
+	import Heading from '$lib/components/Heading.svelte'
+</script>
+
+<section class="container mx-auto pt-6 md:pt-8 px-2 sm:px-4">
+	<Heading level={1}>Builder</Heading>
+	<p>Use this builder to create a carbon.txt file for your organisation or website.</p>
+	<p>Work in progress ...</p>
+</section>
