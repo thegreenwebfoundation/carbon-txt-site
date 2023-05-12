1afa96d5a8fb7d821fb54d14d07af285b4d21320
fershad <27988517+fershad@users.noreply.github.com>
2023-05-12 21:05:32 +0800

    update footer content

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,21 +1,43 @@
-<header class="bg-black border-t-2 mt-[10rem]">
+<header class="bg-white border-t-2 mt-[10rem]">
 	<div class="container mx-auto p-4 sm:px-4">
-		<nav class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+		<nav class="flex gap-[2rem] justify-between items-center flex-wrap py-6 md:justify-start md:space-x-1">
 			<div class="flex-grow">
+				<p class="text-2xl mb-3">Carbon.txt is a</p>
 				<a href="/">
-					<img
-						width="200px"
-						height="48px"
-						src="/tgwf-logo.svg"
-						alt="The Green Web Foundation logo"
-					/>
+					<img width="200px" height="48px" src="/tgwf-logo.svg" alt="Green Web Foundation logo" />
 				</a>
+				<p class="text-2xl mt-3">initiative.</p>
 			</div>
 
-			<div class="pl-10 flex gap-3">
-				<a href="">What is carbon.txt?</a>
-				<a href="">Validator</a>
-				<a href="">Builder</a>
+			<div class="flex gap-[2rem] flex-wrap justify-start">
+				<div class="flex flex-col gap-3">
+					<p class="text-2xl mb-3">Green Web Foundation</p>
+					<ul>
+						<li class="my-3">
+							<a href="https://thegreenwebfoundation.org">Homepage</a>
+						</li>
+						<li class="my-3">
+							<a href="https://github.com/thegreenwebfoundation">Github</a>
+						</li>
+						<li class="my-3">
+							<a href="https://www.linkedin.com/company/green-web-foundation/">LinkedIn</a>
+						</li>
+					</ul>
+				</div>
+				<div class="flex flex-col">
+					<p class="text-2xl mb-3">Carbon.txt</p>
+					<ul>
+						<li class="my-3">
+							<a href="https://carbontxt.org">Homepage</a>
+						</li>
+						<li class="my-3">
+							<a href="https://github.com/thegreenwebfoundation/carbon.txt">Github</a>
+						</li>
+						<li class="my-3">
+							<a href="mailto:fershad@thegreenwebfoundation.org">Support email</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	</div>
