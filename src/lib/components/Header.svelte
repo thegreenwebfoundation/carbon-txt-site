62dead7ce96cbc03e8f2eb9c8e6cbe5dd1b71b52
fershad <27988517+fershad@users.noreply.github.com>
2023-05-12 21:34:04 +0800

    header wraps

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,14 +7,14 @@
 
 <header class="bg-white border-b-2">
 	<div class="container mx-auto px-2 sm:px-4">
-		<nav class="flex justify-between items-center bg-white py-6 md:justify-start md:space-x-10">
-			<div class="flex-grow">
+		<nav class="flex flex-wrap justify-center md:justify-between items-center bg-white py-6 md:space-x-10 gap-[2rem]">
+			<div class="">
 				<a href="/">
 					<img width="200px" height="48px" src="/tgwf-logo.svg" alt="The Green Web Foundation logo" />
 				</a>
 			</div>
 
-			<div class="pl-10 flex gap-3">
+			<div class="flex gap-3 flex-wrap">
 				<a href="/about" class={currentPage('/about')}>What is carbon.txt?</a>
 				<a href="/how/digital-services" class={currentPage('/how/digital-services')}>Get started</a>
 				<a href="/tools/validator" class={currentPage('/tools/validator')}>Validator</a>
