452f6c4fc702fbe3190f8ea7fc4d7b6f022996db
fershad <27988517+fershad@users.noreply.github.com>
2023-05-15 09:44:35 +0800

    change active style

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores'
 
-	$: currentPage = (route = '') => ($page.route.id === route ? 'no-underline text-green-600' : '')
+	$: currentPage = (route = '') => ($page.route.id === route ? 'no-underline text-green-600 font-mono font-bold' : '')
 </script>
 
 <header class="bg-white border-b-2">
