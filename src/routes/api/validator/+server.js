6f01e61d3f4e7cfe9266767ed5975ba8db697eb7
fershad <27988517+fershad@users.noreply.github.com>
2023-05-17 12:11:57 +0800

    two validator endpoints

--- src/routes/api/validator/+server.js
+++ src/routes/api/validator/+server.js
@@ -1,19 +1,36 @@
 import { error } from '@sveltejs/kit';
 
 /** @type {import('../../tools/validator/$types').RequestHandler} */
-export async function GET({ url, platform }) {
-    const toml = `[upstream]
-    providers = [
-        { domain='aws.amazon.com', service = 'infrastructure' },
-        { domain='services.global.ntt', service = 'infrastructure' }
-    ]
-    [org]
-    credentials = [
-        { domain = 'imageengine.io', aliases = ['imgeng.in'], doctype = 'sustainability-page', url = "https://imageengine.io/imageengine-sustainability-policy/" },
-        { domain = 'imageengine.io', aliases = ['imgeng.in'], doctype = 'web page', url = "https://github.com/imgeng/sustainability"},
-    ]`
+export async function POST({ url, platform, request }) {
+    // This function will be sent a POST request with a body that contains a valid TOML string.
+    // It will then save the TOML string to KV with a unique ID and a cache TTL of 10 minutes.
+    // It will then return the ID to the client.
 
-    await platform.env.TEST_KV.put('test.toml', toml);
+    // Get the TOML from the body
+    const toml = await request.text();
+    // Generate a unique ID
+    const id = crypto.randomUUID()
+    // Save the TOML to KV
+    await platform.env.TEST_KV.put(id, toml, { expirationTtl: 600 });
+
+    return new Response(String(id));
+}
+
+export async function GET({ url, platform }) {
+    // This function will be sent a GET request with a query string that is a unique ID.
+    // It will then fetch the TOML string from KV and return it to the client.
+
+    const id = url.searchParams.get('id');
+
+    if (!id) {
+        return error(400, 'Missing ID');
+    }
+
+    const toml = await platform.env.TEST_KV.get(id);
+
+    if (!toml) {
+        return error(404, 'Not found');
+    }
 
     return new Response(String(toml));
 }
