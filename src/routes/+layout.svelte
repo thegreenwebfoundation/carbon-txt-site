ba854ff07e59e181de9c079d5e9dc095610ee3a8
fershad <27988517+fershad@users.noreply.github.com>
2023-05-26 10:53:46 +0800

    remove gradient

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script>
-	import '../app.postcss';
-	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
+	import '../app.postcss'
+	import Header from '$lib/components/Header.svelte'
+	import Footer from '$lib/components/Footer.svelte'
 </script>
 
 <Header />
-<div class="relative block">
-	<div
-		class="block fixed -z-50 left-0 w-1/2 -top-24 bg-no-repeat bg-right bg-[length:100%_100%] bg-gradient-to-r from-green-500"
-		style="height: calc(100% + 100px);"
-	/>
+<main>
 	<slot />
-</div>
+</main>
 <Footer />
