74ec3f1e42a4ab1f2c7f55d397f31e4a460aec04
fershad <27988517+fershad@users.noreply.github.com>
2024-05-24 21:13:46 +0800

    remove sentry

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit(), sentryVitePlugin({
-        org: "fershad-digital",
-        project: "carbontxt"
-    }), sentryVitePlugin({
-        org: "fershad-digital",
-        project: "carbontxt"
-    })],
-
+    plugins: [sveltekit()],
     build: {
         sourcemap: true
     }
