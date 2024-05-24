2c754fa14898eeb23dc59ae085ddbf402cdb2c8b
fershad <27988517+fershad@users.noreply.github.com>
2024-05-24 20:49:52 +0800

    add sentry

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit(), sentryVitePlugin({
+        org: "fershad-digital",
+        project: "carbontxt"
+    }), sentryVitePlugin({
+        org: "fershad-digital",
+        project: "carbontxt"
+    })],
+
+    build: {
+        sourcemap: true
+    }
 });
