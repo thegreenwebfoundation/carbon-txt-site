39f3d8769e41c6b1078f4206c2d6b99c0fbcbe4c
fershad <27988517+fershad@users.noreply.github.com>
2024-05-24 21:10:15 +0800

    remove sentry

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,30 +2,6 @@
 	import '../app.postcss'
 	import Header from '$lib/components/Header.svelte'
 	import Footer from '$lib/components/Footer.svelte'
-
-	import * as Sentry from '@sentry/browser'
-
-	Sentry.init({
-		dsn: 'https://10b7b3fe441fe715d60351db7226816d@o4507311333376000.ingest.de.sentry.io/4507311336652880',
-
-		// Alternatively, use `process.env.npm_package_version` for a dynamic release version
-		// if your build tool supports it.
-		release: 'carbontxt@2.3.12',
-		integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-
-		// Set tracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production
-		tracesSampleRate: 1.0,
-
-		// Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-		tracePropagationTargets: ['localhost', /^https:\/\/carbontxt\.org\//],
-
-		// Capture Replay for 10% of all sessions,
-		// plus for 100% of sessions with an error
-		replaysSessionSampleRate: 0.1,
-		replaysOnErrorSampleRate: 1.0
-	})
 </script>
 
 <Header />
