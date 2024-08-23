8fc595d0b3f23c603224fa16c165ce782798144d
fershad <27988517+fershad@users.noreply.github.com>
2024-08-23 09:17:51 +0800

    update link

--- src/lib/components/NoticeBanner.svelte
+++ src/lib/components/NoticeBanner.svelte
@@ -1,9 +1,6 @@
 <div class="border-2 border-dark-gray alert__warning text-center p-4">
 	<p class="text-sm sm:text-base">
 		We've got big plans for Carbon.txt to improve the discoverability of sustainability data thanks to funding from NGI Search.
-		<br /><a
-			href="https://www.thegreenwebfoundation.org/news/carbon-txt-applying-lessons-from-crowdsourcing-net-zero-data/"
-			title="Introducing carbon.txt – Applying lessons from crowdsourcing net zero data">Read more</a
-		>
+		<br /><a href="https://www.thegreenwebfoundation.org/tools/carbon-txt" title="Introducing carbon.txt – Applying lessons from crowdsourcing net zero data">Read more</a>
 	</p>
 </div>
--- src/lib/components/Pilot.svelte
+++ src/lib/components/Pilot.svelte
@@ -1,9 +1,7 @@
 <aside class="border-2 border-dark-gray rounded-3xl h-100-l p-8 prose alert__warning">
 	<p class="text-2xl">Current status: Research</p>
 	<p>
-		This project is currently in its very early stages. We are <a href="https://www.thegreenwebfoundation.org/news/carbon-txt-applying-lessons-from-crowdsourcing-net-zero-data/"
-			>currently researching</a
-		>
+		This project is currently in its very early stages. We are <a href="https://www.thegreenwebfoundation.org/tools/carbon-txt">currently researching</a>
 		how to expand carbon.txt for this standardised data around green energy, and
 		<a href="https://www.thegreenwebfoundation.org/news/eu-sustainability-regulation-experts-can-you-help-us/">review the protocol with experts</a> to improve it.
 	</p>
