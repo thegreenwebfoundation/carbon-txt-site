1e25567b0e60391c9029ab1a174b225f6b306ec0
fershad <27988517+fershad@users.noreply.github.com>
2024-10-07 21:12:39 +0800

    comment out currently unused code

--- src/routes/tools/validator/+page.server.js
+++ src/routes/tools/validator/+page.server.js
@@ -1,10 +1,5 @@
 import registerFile from '$lib/utils/actions/registerFile';
 
-/** @type {import('@sveltejs/kit').Load} */
-export async function load({ fetch, session }) {
-}
-
-
 /** @type {import('./$types').Actions} */
 export const actions = {
 	registerFile,
