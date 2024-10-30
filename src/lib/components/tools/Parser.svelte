baba6c536374fdbe88e8df137b9b461ed54e54dc
fershad <27988517+fershad@users.noreply.github.com>
2024-10-30 21:23:47 +0800

    skeleton parser component

---
+++ src/lib/components/tools/Parser.svelte
@@ -0,0 +1,12 @@
+<script>
+	
+</script>
+
+<!-- TODO: This is a skeleton. Spoooky ~~ -->
+<form>
+	<!-- A text input that takes a valid website domain -->
+	<label for="domain">Domain</label>
+	<input type="text" id="domain" name="domain" placeholder="example.com" required>
+	<!-- A submit button that sends the domain to the parser -->
+	<button type="submit">Parse</button>
+</form>
