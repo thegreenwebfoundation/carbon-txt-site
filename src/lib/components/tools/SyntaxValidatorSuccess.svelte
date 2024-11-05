294e4e2400b0ba5c2b2306fb7019e587ae8c73ce
fershad <27988517+fershad@users.noreply.github.com>
2024-11-05 12:25:10 +0800

    create basic success message

---
+++ src/lib/components/tools/SyntaxValidatorSuccess.svelte
@@ -0,0 +1,12 @@
+<script>
+	let { text_content } = $props()
+	import Code from '$lib/components/Code.svelte'
+</script>
+
+<section class="w-100" id="result">
+	<div class="prose md:w-[80%]">
+		<h1 class="text-3xl font-bold mb-4">Success!</h1>
+		<p>Your carbon.txt file is syntactically valid.</p>
+	</div>
+	<Code lang="toml" code={text_content} />
+</section>
