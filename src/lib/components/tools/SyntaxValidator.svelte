44ee5641795b42019790de1226b96dd821700e46
fershad <27988517+fershad@users.noreply.github.com>
2024-11-10 21:19:39 +0800

    adjust name

--- src/lib/components/tools/SyntaxValidator.svelte
+++ src/lib/components/tools/SyntaxValidator.svelte
@@ -5,6 +5,6 @@
 </script>
 
 <form class="validator-holder relative" method="POST" action="/tools/validator?/validate" use:enhance>
-	<textarea name="carbon-txt" bind:value={textInput} rows="15" />
+	<textarea name="carbon-txt-validator" bind:value={textInput} rows="15" />
 	<button type="submit" class="btn btn-white w-[100%]">Submit</button>
 </form>
