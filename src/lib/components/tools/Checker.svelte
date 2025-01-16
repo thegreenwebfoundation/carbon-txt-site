c061f50234377a8da55f4a9cb6e15fcc3ae2964e
fershad <27988517+fershad@users.noreply.github.com>
2025-01-16 10:59:28 +0800

    add spinner

--- src/lib/components/tools/Checker.svelte
+++ src/lib/components/tools/Checker.svelte
@@ -1,15 +1,36 @@
 <script>
 	import { enhance } from '$app/forms'
+	let creating = $state(false)
 
 	let { textInput } = $props()
 </script>
 
-<form class="validator-holder relative" method="POST" action="/tools/checker?/check" use:enhance>
+<form
+	class="validator-holder relative"
+	method="POST"
+	action="/tools/checker?/check"
+	use:enhance={() => {
+		creating = true
+
+		return async ({ update }) => {
+			await update()
+			creating = false
+		}
+	}}
+>
 	<!-- A text input that takes a valid website domain -->
 	<label for="domain">Carbon.txt File URL</label>
 	<input type="url" id="carbon-txt-url" name="carbon-txt-url" placeholder="https://example.com/carbon.txt" required bind:value={textInput} pattern="https?:\/\/[a-z0-9-]+(\.[a-z0-9-]+)+([/?].*)" />
-	<!-- A submit button that sends the domain to the parser -->
-	<button type="submit" class="btn btn-white w-[100%]">Fetch file and check</button>
+	<span aria-live="assertive" data-checking={creating}>
+		{#if creating}
+			<div class="checker">
+				<span></span>
+				<p>Checking carbon.txt file ....</p>
+			</div>
+		{:else}
+			<button type="submit" class="btn btn-white w-[100%]">Fetch file and check</button>
+		{/if}
+	</span>
 </form>
 
 <style>
@@ -17,5 +38,33 @@
 		display: grid;
 		grid-template-columns: 1fr;
 		grid-gap: 1rem;
+	}
+
+	.checker {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 1.25rem;
+	}
+
+	.checker span {
+		display: inline-block;
+		width: 1.5rem;
+		height: 1.5rem;
+		border-radius: 50%;
+		border: 2px solid #333;
+		border-top-color: transparent;
+		border-right-color: transparent;
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
 	}
 </style>
