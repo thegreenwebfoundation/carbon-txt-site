01f46201269543f269aa060b906e4cb13ed16829
fershad <27988517+fershad@users.noreply.github.com>
2025-01-21 20:09:50 +0800

    separate errors and data

--- src/lib/components/tools/SyntaxValidatorSuccess.svelte
+++ src/lib/components/tools/SyntaxValidatorSuccess.svelte
@@ -4,6 +4,22 @@
 	import Code from '../Code.svelte'
 
 	const pluralisePlugins = (count) => (count === 1 ? 'plugin' : 'plugins')
+
+	const pluginResponse = () => {
+		if (form?.response.document_data && Object.keys(form?.response.document_data).length > 0 && Object.keys(form?.response.document_data)[0] !== 'null') {
+			return Object.keys(form?.response.document_data).map((plugin) => {
+				return {
+					name: plugin,
+					data: {
+						success: form?.response.document_data[plugin].filter((item) => !item.error),
+						error: form?.response.document_data[plugin].filter((item) => item.error)
+					}
+				}
+			})
+		}
+	}
+
+	const pluginData = pluginResponse() || []
 </script>
 
 <section class="w-100" id="result">
@@ -74,49 +90,27 @@
 				<Heading level={2}>Linked evidence</Heading>
 				<p>Found linked evidence using <b>{Object.keys(form?.response.document_data).length} {pluralisePlugins(Object.keys(form?.response.document_data).length)}</b>.</p>
 
-				{#each Object.keys(form?.response.document_data) as plugin}
+				{#each pluginData as plugin}
 					<div class="relative overflow-x-auto max-w-[100vw]">
-						<table class="w-full">
-							<thead>
-								<tr>
-									<td colspan="7">Plugin: {plugin}</td>
-								</tr>
-								<tr>
-									<th>Status</th>
-									<th>Evidence name</th>
-									<th>Value</th>
-									<th>Unit</th>
-									<th>Start date</th>
-									<th>End date</th>
-									<th>Source file</th>
-								</tr>
-							</thead>
-							<tbody>
-								{#each form?.response.document_data[plugin] as { name, value, unit, start_date, end_date, file, message, error, datapoint_short_code, datapoint_readable_label }}
-									{#if error}
-										<tr data-error>
-											<td class="whitespace-nowrap">
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													width="24"
-													height="24"
-													stroke-width="2"
-												>
-													<path
-														d="M19.875 6.27c.7 .398 1.13 1.143 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"
-													></path> <path d="M12 16v.01"></path> <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
-												</svg>
-												Not found</td
-											>
-											<td class="whitespace-nowrap">{datapoint_readable_label}</td>
-											<td class="whitespace-nowrap" colspan="5">{message}</td>
-										</tr>
-									{:else}
+						{#if plugin.data.success.length > 0}
+							<Heading level={3}>Data found</Heading>
+							<table class="w-full">
+								<thead>
+									<tr>
+										<td colspan="7">Plugin: {plugin.name}</td>
+									</tr>
+									<tr>
+										<th>Status</th>
+										<th>Evidence name</th>
+										<th>Value</th>
+										<th>Unit</th>
+										<th>Start date</th>
+										<th>End date</th>
+										<th>Source file</th>
+									</tr>
+								</thead>
+								<tbody>
+									{#each plugin.data?.success as { name, value, unit, start_date, end_date, file }}
 										<tr>
 											<td class="whitespace-nowrap">
 												<svg
@@ -141,10 +135,52 @@
 											<td class="whitespace-nowrap"><time datetime={end_date}>{end_date}</time></td>
 											<td class="whitespace-nowrap"><a href={file}>{file}</a></td>
 										</tr>
-									{/if}
-								{/each}
-							</tbody>
-						</table>
+									{/each}
+								</tbody>
+							</table>
+						{/if}
+
+						{#if plugin.data.error.length > 0}
+							<Heading level={3}>Errors</Heading>
+							<table class="w-full">
+								<thead>
+									<tr>
+										<td colspan="7">Plugin: {plugin.name}</td>
+									</tr>
+									<tr>
+										<th>Status</th>
+										<th>Evidence name</th>
+										<th>Message</th>
+									</tr>
+								</thead>
+								<tbody>
+									{#each plugin.data?.error as { message, error, datapoint_short_code, datapoint_readable_label }}
+										<tr data-error>
+											<td class="whitespace-nowrap">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 24 24"
+													fill="none"
+													stroke="currentColor"
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													width="24"
+													height="24"
+													stroke-width="2"
+												>
+													<path
+														d="M19.875 6.27c.7 .398 1.13 1.143 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"
+													></path> <path d="M12 16v.01"></path> <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
+												</svg>
+												Not found</td
+											>
+											<td class="whitespace-nowrap">{datapoint_readable_label}</td>
+											<td class="whitespace-nowrap"><span class="prose text-wrap max-w-prose">{message}</span></td>
+										</tr>
+									{/each}
+								</tbody>
+							</table>
+						{/if}
 					</div>
 				{/each}
 			</div>
@@ -193,24 +229,5 @@
 	td svg {
 		display: inline-block;
 		margin-right: 0.5rem;
-	}
-
-	/* tr[data-error] {
-		@apply bg-red-50;
-	} */
-
-	tr > td:first-of-type,
-	tr > th:first-of-type {
-		position: sticky;
-		left: 0;
-		z-index: 1;
-	}
-
-	tr > td:first-of-type {
-		background-color: white;
-	}
-
-	tr > th:first-of-type {
-		background-color: bg-purple-100;
 	}
 </style>
