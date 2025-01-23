66f670137a984aff96a3ccc68d5af9cea6cd40f7
fershad <27988517+fershad@users.noreply.github.com>
2025-01-23 12:34:26 +0800

    update api fetch

--- src/lib/utils/evidenceTypes.js
+++ src/lib/utils/evidenceTypes.js
@@ -2,10 +2,14 @@ const fetchEvidenceTypes = async () => {
     const schema = await fetch('https://carbon-txt-api.greenweb.org/api/json_schema/')
     const json = await schema.json()
 
-    return json.$defs.Credential.properties.doctype.enum.map((type) => {
+    return json.$defs.Disclosure.properties.doc_type.enum.map((type) => {
+        let name = type.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase())
+        if (name.startsWith('Csrd')) {
+            name = name.replace('Csrd', 'CSRD')
+        }
         return {
             // Make the name a human-readable string with spaces and capitalization
-            name: type.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase()),
+            name: name,
             slug: type
         }
     })
