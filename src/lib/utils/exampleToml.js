571fa9828ceab21c984c67e5d664cece41b9258a
fershad <27988517+fershad@users.noreply.github.com>
2025-01-23 13:43:18 +0800

    update example

--- src/lib/utils/exampleToml.js
+++ src/lib/utils/exampleToml.js
@@ -2,8 +2,8 @@ export const digitalServiceToml = `[org]
 # Optional.
 # An array of documents that point to evidence of green claims made by mycompany.com.
 credentials = [
-	{ domain = "mycompany.com", doctype = "webpage", url = "https://mycompany.com/sustainability" },
-	{ domain = "mycompany.com", doctype = "annual-report", url = "https://mycompany.com/carbon-emissions-2022.pdf" }
+	{ doctype = "webpage", url = "https://mycompany.com/sustainability", domain = "mycompany.com" },
+	{ doctype = "annual-report", url = "https://mycompany.com/carbon-emissions-2022.pdf", domain = "mycompany.com" }
 ]
 	
 [upstream]	
