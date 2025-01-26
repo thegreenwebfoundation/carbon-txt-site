05f909dedb14c728b17a90c4ced784fd325dfd06
fershad <27988517+fershad@users.noreply.github.com>
2025-01-26 20:51:03 +0800

    remove pilot text,

--- src/lib/components/content/What.svelte
+++ src/lib/components/content/What.svelte
@@ -1,6 +1,4 @@
 <script>
-	import Heading from '$lib/components/Heading.svelte'
-	import Pilot from '$lib/components/Pilot.svelte'
 </script>
 
 <div class="grid lg:grid-cols-2 lg:items-center gap-10">
@@ -8,5 +6,4 @@
 		A proposed convention for website owners and digital service providers to demonstrate that their digital infrastructure runs on green electricity. This is achieved by reusing existing governance
 		structures, already published data, and existing industry standards as much possible.
 	</p>
-	<Pilot />
 </div>
