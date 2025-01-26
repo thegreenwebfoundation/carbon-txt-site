7f1641ba74daa009e2ea680bc4d9aef9729c8b15
fershad <27988517+fershad@users.noreply.github.com>
2025-01-26 20:51:27 +0800

    add new summary

--- src/lib/components/content/What.svelte
+++ src/lib/components/content/What.svelte
@@ -3,7 +3,12 @@
 
 <div class="grid lg:grid-cols-2 lg:items-center gap-10">
 	<p class="prose text-inherit">
-		A proposed convention for website owners and digital service providers to demonstrate that their digital infrastructure runs on green electricity. This is achieved by reusing existing governance
-		structures, already published data, and existing industry standards as much possible.
+		carbon.txt makes sustainability data easier to discover and use on the web. Carbon.txt is a single place to look on any domain – /carbon.txt – for public, machine-readable, sustainability data
+		relating to that company.
+	</p>
+
+	<p class="prose text-inherit">
+		It’s a web-first, connect not collect style approach, of most benefit to those interested in scraping the structured data companies have to publish according to national laws. Designed to be
+		extended by default, we see carbon.txt becoming essential infrastructure for sustainability data services crunching available numbers and sharing the stories it can tell.
 	</p>
 </div>
