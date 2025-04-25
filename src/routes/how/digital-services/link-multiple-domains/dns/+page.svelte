0f6a60261c18d72cffbfa30b418a117ff5758282
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2025-04-25 12:01:56 +0200

    Update documentation for HTTP and DNS delegation
    In order to match the changes introduced by [ADR 02 - Improved architecture for domain validation and carbon.txt delegation](https://github.com/thegreenwebfoundation/carbon-txt-validator/blob/tc-adr-2-improvements-to-delegation-and-domain-validation/docs/adrs/02_improvements_to_delegation_and_domain_validation.md)
    
    This changes the names of the http header and dns txt record used, and removes all mentions of domain hashes.

--- src/routes/how/digital-services/link-multiple-domains/dns/+page.svelte
+++ src/routes/how/digital-services/link-multiple-domains/dns/+page.svelte
@@ -1,17 +1,11 @@
 <script>
 	import Heading from '$lib/components/Heading.svelte'
 	import Code from '$lib/components/Code.svelte'
-	import DomainHash from '$lib/components/tools/DomainHash.svelte'
-	import copy from 'clipboard-copy'
-	export let form
-
-	let copyText = 'Copy to clipboard'
-	const example = form?.result ? `TXT "carbon-txt=https://my-org.com/carbon.txt ${form.result}"` : 'TXT "carbon-txt=https://my-org.com/carbon.txt <generated_domain_hash>"'
 </script>
 
 <article class="container mx-auto pt-6 md:pt-8 px-2 sm:px-4">
 	<div class="w-100 mb-[10rem] prose">
-		<Heading level={1}>How to link two domains using DNS TXT records and domain hashes</Heading>
+		<Heading level={1}>How to link two domains using DNS TXT records</Heading>
 		<p>DNS text records are frequently used to help organisations demonstrate they have control over a domain. The DNS TXT record approach follows similar principles.</p>
 		<p>To do this:</p>
 		<ol class="flex flex-col gap-3 p-0">
@@ -19,46 +13,14 @@
 				<Heading level={2}>Follow the steps above to create your carbon.txt file</Heading>
 				<p>Follow Steps 1 to 3 of the <a href="/quickstart">Getting Started guide</a> above to create a carbon.txt file for your organisation.</p>
 			</li>
-			<li class="border-t border-gray-400" id="create-domain-hash">
-				<Heading level={2}>Create a domain hash for the domain you want to show as green</Heading>
-				<p>Create a domain hash. This is a SHA256 hash of your shared secret and the domain you want to establish a link to.</p>
-				<p>Use the form below to create one.</p>
-
-				{#if form?.body}
-					<div class="alert__error">
-						{form.body}
-					</div>
-					<DomainHash />
-				{:else if form?.result}
-					<div class="alert__success">
-						<p>Success, your domain hash has been generated!</p>
-						<Code code={form.result} lang={'txt'} />
-					</div>
-					<button
-						class="btn mx-auto min-w-[20ch] block mx-auto"
-						on:click={() => {
-							const copySuccess = copy(form.result)
-							// Check if promise resolves to true
-							if (copySuccess) {
-								copyText = '🎉 Copied!'
-								setTimeout(() => {
-									copyText = 'Copy to clipboard'
-								}, 2000)
-							}
-						}}>{copyText}</button
-					>
-				{:else}
-					<DomainHash />
-				{/if}
-			</li>
 			<li class="border-t border-gray-400">
-				<Heading level={2}>Set a DNS record for the domains you want to link back to your main carbon.txt, containing the domain hash</Heading>
+				<Heading level={2}>Set a DNS record for the domains you want to link back to your main carbon.txt</Heading>
 				<p>
-					Add the generated domain hash as a final, optional part of the DNS TXT record defining the domain you want to link back to your main carbon.txt file, along with the location of the
-					carbon.txt file and the generated hash.
+					Define a DNS TXT record for the domain you want to link back to your main carbon.txt file, with the location of the
+					carbon.txt file prefixed by <code>carbon-txt-location=</code>.
 				</p>
 				<p>For example: my-org.com also owns me.my-org.com. In order to link me.my-org.com to the main carbon.txt file, they would create a TXT record that looks something like:</p>
-				<Code code={example} lang="http" />
+				<Code code='TXT "carbon-txt-location=https://my-org.com/carbon.txt"' lang="http" />
 				<p>
 					<b>Note:</b> You can see an example DNS TXT record for the domain <a href="https://delegating-with-txt-record.carbontxt.org">delegating-with-txt-record.carbontxt.org</a> using online tools
 					like
--- src/routes/how/digital-services/link-multiple-domains/via/+page.svelte
+++ src/routes/how/digital-services/link-multiple-domains/via/+page.svelte
@@ -1,68 +1,27 @@
 <script>
 	import Heading from '$lib/components/Heading.svelte'
 	import Code from '$lib/components/Code.svelte'
-	import DomainHash from '$lib/components/tools/DomainHash.svelte'
-	import copy from 'clipboard-copy'
-	export let form
-
-	let copyText = 'Copy to clipboard'
-	const example = form?.result ? `Via: 1.1 https://my-org.com/carbon.txt ${form.result}"` : 'Via: 1.1 https://my-org.com/carbon.txt <generated_domain_hash>'
 </script>
 
 <article class="container mx-auto pt-6 md:pt-8 px-2 sm:px-4">
 	<div class="prose">
-		<Heading level={1}>How to link two domains using HTTP Via headers domain hashes</Heading>
+		<Heading level={1}>How to link two domains using an HTTP header</Heading>
 		<p>HTTP requests and responses can contain a number of extra headers, which you can use to send along extra metadata about the server serving the request.</p>
 		<p>
-			Carbon.txt supports using the <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/via">HTTP Via header</a>, to declare that a HTTP response has been sent along by one or more
-			intermediate entities. In our case usually a managed hosting provider.
+			Carbon.txt supports using the <code>CarbonTxt-Location</code> header, to point to a carbon.txt served on another domain,  In our case usually a managed hosting provider.
 		</p>
 		<ol class="flex flex-col gap-3 p-0">
 			<li class="border-t border-gray-400">
 				<Heading level={2}>Follow the steps above to create your carbon.txt file</Heading>
 				<p>Follow Steps 1 to 3 of the <a href="/quickstart">Getting Started guide</a> above to create a carbon.txt file for your organisation.</p>
 			</li>
-			<li class="border-t border-gray-400" id="create-domain-hash">
-				<Heading level={2}>Create a domain hash for the domain you want to show as green</Heading>
-				<p>Create a domain hash. This is a SHA256 hash of your shared secret and the domain you want to establish a link to.</p>
-
-				<p>Use the form below to create one.</p>
-
-				{#if form?.body}
-					<div class="alert__error">
-						{form.body}
-					</div>
-					<DomainHash />
-				{:else if form?.result}
-					<div class="alert__success">
-						<p>Success, your domain hash has been generated!</p>
-						<Code code={form.result} lang={'txt'} />
-					</div>
-					<button
-						class="btn mx-auto min-w-[20ch] block mx-auto"
-						on:click={() => {
-							const copySuccess = copy(form.result)
-							// Check if promise resolves to true
-							if (copySuccess) {
-								copyText = '🎉 Copied!'
-								setTimeout(() => {
-									copyText = 'Copy to clipboard'
-								}, 2000)
-							}
-						}}>{copyText}</button
-					>
-				{:else}
-					<DomainHash />
-				{/if}
-			</li>
 			<li class="border-t border-gray-400">
-				<Heading level={2}>Set the <code>via</code> header on HTTP responses to requests for the domain you want to show as green</Heading>
+				<Heading level={2}>Set the header on HTTP responses from the domain you want to show as green</Heading>
 				<p>
 					For example: my-org.com also owns me.my-org.com. In order to link me.my-org.com to the main carbon.txt file, when a request comes in for me.my-org.com, you would configure the server serving
-					the request to add the following Via header.
+					the request to add the following header.
 				</p>
-				<Code lang="http" code={example} />
-				<p><b>Note</b>: the domain hash would be a 64 character hash of me.my-org.com and your shared secret.</p>
+				<Code lang="http" code="CarbonTxt-Location: https://my-org.com/carbon.txt" />
 				<p>
 					We maintain a set of <a href="https://github.com/thegreenwebfoundation/carbon.txt/tree/master/examples">server config setup examples folder on github</a>, for popular open source servers
 					like Nginx, Caddy, Apache, and so on (pull requests gratefully accepted).
