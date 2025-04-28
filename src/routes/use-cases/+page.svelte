a0a55a3fb2c1cdce9430e95739340a6994f766e8
hanopcan <hannah@opcan.co.uk>
2025-04-28 10:54:07 +0100

    Next iteration of use case page

--- src/routes/use-cases/+page.svelte
+++ src/routes/use-cases/+page.svelte
@@ -20,16 +20,27 @@
 	<div class="w-100 mb-[5rem] pt-[2.5rem]">
 		<Heading level={1}>Use cases</Heading>
 		<What short="true"/>
-		<p class="prose mt-4">Here's how you can put to carbon.txt to use in your own organisation.</p>
+		<p class="prose my-4">Here's some practical ways you can put to carbon.txt to use in your own organisation.</p>
 
-		<ul class="prose list-disc m-6">
-			<li>
-				<a href="https://www.thegreenwebfoundation.org/news/using-carbon-txt-with-tcs/">
-				Using carbon.txt with the Technology Carbon Standard</a>
+		<ul class="prose ml-0">
+			<li class="py-3 pl-0">
+				<a class="font-bold" href="https://www.thegreenwebfoundation.org/news/using-carbon-txt-with-csrd-reports/">
+				Using carbon.txt with CSRD reports</a> - how carbon.txt can be used to share Corporate Sustainability Reporting Directive 
+				(CSRD) reports and find specific sustainability data points within them.
+			</li>	
+
+			<li class="py-3 pl-0">
+				<a class="font-bold"  href="https://www.thegreenwebfoundation.org/news/using-carbon-txt-with-tcs/">
+				Using carbon.txt with the Technology Carbon Standard</a> - how carbon.txt can be combined with the Technology 
+				Carbon Standard (TCS) to create greater transparency in the field of digital sustainability. We cover how we collaborated
+				 with the sustainability team at Scott Logic, the driving force behind TCS, to simplify digital estate emissions reports through developing an initial TCS Schema.
 			</li>
-			<li>
-				<a href="https://www.thegreenwebfoundation.org/news/green-hosting-providers-can-use-carbon-txt/">
-				Green hosting providers can use carbon.txt</a>
+
+			<li class="py-3 pl-0">
+				<a class="font-bold"  href="https://www.thegreenwebfoundation.org/news/green-hosting-providers-can-use-carbon-txt/">
+				Green hosting providers can use carbon.txt</a> - how we’re using carbon.txt, in combination with our Green Web Dataset. 
+				We cover why sharing evidence of green claims data inside a carbon.txt file has the potential to positively impact organisations 
+				disclosing data and the digital sustainability sector as a whole.
 			</li>
 		</ul>
 
