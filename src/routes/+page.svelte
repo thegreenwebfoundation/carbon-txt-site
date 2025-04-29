174748e7db005adf79cbbbd96b25604804a75539
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2025-04-29 12:57:54 +0200

    tidy up RHS gutter on home page at large breakpoints - align video with right edge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,17 +29,19 @@
 			<div class="py-8">
 				<Heading level={1}>Sustainability data that’s easy to discover and use.</Heading>
 				<div class="mb-10 prose">
-					<p class="">Carbon.txt is a single recognisable location on any web domain for public sustainability data relating 
+					<p class="">Carbon.txt is a single recognisable location on any web domain for public sustainability data relating
 					to that company. The open source ecosystem of syntax, apis & plugins is free to use and build on.</p>
-					<p class="mt-4">Be part of the movement for open, discoverable, comparable, 
+					<p class="mt-4">Be part of the movement for open, discoverable, comparable,
 					and traceable sustainability data on the web.</p>
 				</div>
 			</div>
-			<div class="max-w-100">
-				<img class="m-auto" width="600px" height="308px" src="/carbontxt-video-cover.jpg" alt="Hannah Smith presenting carbon.txt at State of the Open Con" />
-				<p class="prose my-4 text-center">
-					<a href="https://www.youtube.com/watch?v=55cjE8B-wBM&list=PL0U2cL1JGPZcU-k-4_RT7url83zFdJRug&index=9">
-					Watch our presentation of carbon.txt at State of the Open, London on YouTube</a>.</p>
+			<div class="max-w-100 flex justify-center xl:justify-end">
+        <div>
+          <img width="600px" height="308px" src="/carbontxt-video-cover.jpg" alt="Hannah Smith presenting carbon.txt at State of the Open Con" />
+          <p class="prose my-4 text-center">
+          <a href="https://www.youtube.com/watch?v=55cjE8B-wBM&list=PL0U2cL1JGPZcU-k-4_RT7url83zFdJRug&index=9">
+          Watch our presentation of carbon.txt at State of the Open, London on YouTube</a>.</p>
+        </div>
 			</div>
 		</div>
 
@@ -54,7 +56,7 @@
 <section id="what-data" class="w-100 bg-neutral-900">
 	<div class="container mx-auto p-6 md:pb-[4rem] md:pt-[4rem] px-2 sm:px-4 text-white">
 		<WhatData />
-		
+
 		<div class="text-center">
 			<p>Our free and easy to use tools are there to help you get started.</p>
 
@@ -74,7 +76,7 @@
 
 <section id="cta" class="w-100 bg-neutral-900">
 	<div class="container mx-auto p-6 md:pb-[4rem] md:pt-[4rem] px-2 sm:px-4 text-white text-center flex flex-col justify-center gap-3">
-		
+
 		<Heading level={3}>Get started</Heading>
 		<div class="flex flex-wrap gap-3 w-100 justify-center">
 			<Button link="/quickstart">Follow the guide</Button>
