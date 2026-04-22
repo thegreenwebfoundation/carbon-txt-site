f71de8d91084970552a990bdd0ce4d8e0ad7f002
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2026-04-22 21:19:21 +0200

    tweak spacing, layout, add loading spinner

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -10,19 +10,32 @@
   const WP_CATEGORY_ID = publicEnv["PUBLIC_WP_CATEGORY_ID"] || 93;
 
   onMount(async function() {
-    const response = await fetch(`https://${WP_BLOG_HOST}/wp-json/wp/v2/posts?categories=${WP_CATEGORY_ID}&per_page=${PER_PAGE}&_embed`);
+    const response = await fetch(`https://${WP_BLOG_HOST}/wp-json/wp/v2/posts?categories=${WP_CATEGORY_ID}&per_page=${PER_PAGE}`);
     const json = await response.json()
     const postsData = json.map((postJson) => {
       const title = postJson.title.rendered;
       const link = postJson.link;
-      const authors = postJson._embedded.author.map((authorJson) => (authorJson.name)).join(", ");
       const time = Intl.DateTimeFormat(
         navigator.language,
         { weekday: 'long', month: 'short', day: 'numeric' }
       ).format(new Date(postJson.date));
-      const excerpt = postJson.excerpt.rendered;
-      return { title, link, authors, time, excerpt };
+      const excerptSource = postJson.excerpt.rendered;
+      const excerptDoc = new DOMParser().parseFromString(excerptSource, "text/html")
+      const excerptNode = excerptDoc.querySelector("p");
+      const dateNode = excerptDoc.createElement("strong")
+      dateNode.innerText = `${time}: `;
+      excerptNode.prepend(dateNode);
+      excerptNode.classList.add("mt-0")
+      const readMoreNode = excerptDoc.createElement("a")
+      readMoreNode.href = link;
+      readMoreNode.target = "_blank";
+      readMoreNode.innerText = "[read more]";
+      excerptNode.append(" ");
+      excerptNode.append(readMoreNode);
+      const excerpt = excerptNode.outerHTML;
+      return { title, link, excerpt };
     });
+    document.getElementById("loading-spinner-container").remove()
     posts.set(postsData);
   });
 
@@ -31,20 +44,16 @@
 <section class="container mx-auto pt-6 md:pt-8 px-2 sm:px-4 border-b-2 last:border-0 border-neutral-200">
 	<div class="w-100 mb-[5rem] pt-[2.5rem]">
 		<Heading level={1}>News</Heading>
-		<p class="prose my-4">Read the latest news about the carbon.txt project from the <a href="https://{WP_BLOG_HOST}/news/" target="_blank">Green Web Foundation blog</a>.</p>
+		<p class="prose mt-4 mb-8">Read the latest news about the carbon.txt project from the <a href="https://{WP_BLOG_HOST}/news/" target="_blank">Green Web Foundation blog</a>.</p>
+    <div class="prose" id="loading-spinner-container">
+      <img src="/loading.gif" alt="Loading..." class="mx-auto my-8 py-8" height="75" width="75" />
+    </div>
     {#if $posts && $posts.length > 0}
 		  <ul class="prose ml-0">
       {#each $posts as post}
 			<li class="pt-3 pb-5 pl-0">
-        <Heading level={3} class="pb-1 pt-1"><a href="{post.link}" class="no-underline hover:underline" target="_blank">{@html post.title}</a></Heading>
-        <div class="meta">
-          {#if post.authors.length > 0}
-            by <strong>{post.authors}</strong>, on
-          {/if}
-          <strong>{post.time}</strong>.
-        </div>
+        <Heading level={3} class="pb-1 pt-1 mt-1 mb-0"><a href="{post.link}" class="no-underline hover:underline" target="_blank">{@html post.title}</a></Heading>
         <div class="excerpt">{@html post.excerpt}</div>
-        <div class="link"><a href="{post.link}" target="_blank">Read more...</a></div>
       </li>
       {/each}
 		</ul>
