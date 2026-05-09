5770111b8d905a4498305284b50de58bb616348b
fershad <27988517+fershad@users.noreply.github.com>
2026-05-09 18:59:13 +0800

    add cache headers to response

--- src/routes/news/+page.server.js
+++ src/routes/news/+page.server.js
@@ -4,7 +4,7 @@ const WP_CATEGORY_ID = 93
 const fetchUrl = `http://${WP_BLOG_HOST}/wp-json/wp/v2/posts?categories=${WP_CATEGORY_ID}&per_page=${PER_PAGE}`
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ fetch }) {
+export async function load({ fetch, setHeaders }) {
 	const response = await fetch(fetchUrl)
 	const json = await response.json()
 
@@ -15,6 +15,11 @@ export async function load({ fetch }) {
 		const excerpt = postJson.excerpt.rendered
 
 		return { title, link, time, excerpt }
+	})
+
+	// Cache for 24 hours
+	setHeaders({
+		'cache-control': 'public, max-age=86400'
 	})
 
 	return {
